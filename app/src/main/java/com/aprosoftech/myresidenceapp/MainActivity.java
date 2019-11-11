502553c4314d670d402260c7307d79c16e9b203f
Charanjit Singh <bhallacharanjit@gmail.com>
2019-11-11 10:02:55 +0530

    Updated 11 November 2019

--- app/src/main/java/com/aprosoftech/myresidenceapp/MainActivity.java
+++ app/src/main/java/com/aprosoftech/myresidenceapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.aprosoftech.myresidenceapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,7 +12,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn_1;
+    Button btn_1,btn2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +25,39 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this, "This is my first toast", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+        btn2 = (Button) findViewById(R.id.btn2);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+                alert.setTitle("Confirmation");
+                alert.setMessage("Do you really want to delete this item?");
+
+                alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, "Your item has been deleted", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
+
+                alert.setNegativeButton("No",null);
+
+                alert.setNeutralButton("Neutral",null);
+
+                alert.setCancelable(false);
+
+                alert.show();
+
+
+
             }
         });
 
